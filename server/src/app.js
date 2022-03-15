50a083d8ae7f3d78091d4c8a7a1a8aa7c3103a25
Anthony Zampino <antzamp@Anthonys-MacBook-Pro.local>
2022-03-15 13:47:59 -0400

    Serve react.js frontend in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 // npm imports
 const express = require('express')
 const cors = require('cors')
@@ -11,9 +13,14 @@ const app = express();
 // Middleware
 app.use(cors({
     origin: 'http://localhost:3000'
-}))
-app.use(express.json());
-app.use(planetsRouter);
+}));
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app
