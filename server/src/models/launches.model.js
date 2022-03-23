14e520ea527a949a6458df04dd33b6c09e1442a0
Anthony Zampino <antzamp@Anthonys-MBP.hitronhub.home>
2022-03-23 10:11:23 -0400

    added launches model

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,17 @@
+const launches = new Map();
+
+const launch = {
+    flightNumber: 100,
+    mission: 'Kepler Exploration X',
+    launchDate: new Date('December 27, 2030'),
+    destination: 'Kepler-442 b',
+    customers: ['ZTM', 'NASA'],
+    upcoming: true,
+    success: true,
+}
+
+launches.set(launch.flightNumber, launch)
+
+module.exports = {
+    launches,
+}
