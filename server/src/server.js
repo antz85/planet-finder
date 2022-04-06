77e1396d725df129183801fb70eb1b6e0caa446b
Anthony Zampino <antzamp@Anthonys-MacBook-Pro.local>
2022-04-05 21:44:22 -0400

    connect MongoDB database

--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,32 @@
 // Global imports
-const http = require('http')
+const http = require('http');
 
 // Local imports
-const app = require('./app')
-const {loadPlanetsData} = require("./models/planets.model");
+const mongoose = require('mongoose');
+const app = require('./app');
+const { loadPlanetsData } = require("./models/planets.model");
 
 // Variables
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:cBG6P8PBSVVspfXQ@nasacluster.qfsij.mongodb.net/nasa?retryWrites=true&w=majority';
+
 // Create server
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection is ready');
+})
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
     server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`)
-    })
+        console.log(`Listening on port ${PORT}`);
+    });
 }
 
 startServer();
